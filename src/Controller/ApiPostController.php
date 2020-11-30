647f672cba1cc2637652372fe08a2a769e305edc
BadTripHD <baptistepollet7@gmail.com>
2020-11-30 08:52:20 +0100

    TEST SERIALIZER

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -6,16 +6,21 @@ use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizableInterface;
 
 class ApiPostController extends AbstractController
 {
     /**
      * @Route("/api/post", name="api_post_index", methods={"POST"})
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, NormalizableInterface $normalizable): Response
     {
 
         $posts = $categoryRepository->findAll();
+
+        $postsNormalises = $normalizable->normalize($posts);
+
+        dd($postsNormalises);
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
